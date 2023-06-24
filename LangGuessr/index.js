041cf53dab1b94510bab34b88bcc05ed73895b06
RChipman <rschipman@gmail.com>
2023-06-24 01:53:29 -0300

    tiny fix

--- LangGuessr/index.js
+++ LangGuessr/index.js
@@ -1,14 +1,19 @@
 // REMEMBER TO RUN "npm install" in your terminal (in the same directory as this file)
 const franc = require("franc");
 const langs = require("langs");
-const colors = require("colors")
+const colors = require("colors");
 
 const input = process.argv[2];
 const langCode = franc(input);
-if (langCode === 'und') {
-    console.log("SORRY, COULDN'T FIGURE IT OUT! TRY WITH MORE SAMPLE TEXT!".red)
+if (langCode === "und") {
+  console.log("SORRY, COULDN'T FIGURE IT OUT! TRY WITH MORE SAMPLE TEXT!".red);
 } else {
-    const language = langs.where("3", langCode);
-    console.log(`Our best guess is: ${language.name}`.green)
+  const language = langs.where("3", langCode);
+  if (language) {
+    console.log(colors.green(`Our best guess is: ${language.name}`));
+  } else {
+    console.log(
+      colors.red(`SORRY, COULDN'T FIND A LANGUAGE FOR CODE: ${langCode}`)
+    );
+  }
 }
-
